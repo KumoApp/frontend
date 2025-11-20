dc4a669cd62c5bb1b0d1ecc7873a3903da5e66c4
Mikel_Bracamonte <mikel.bracamonte@utec.edu.pe>
2025-11-19 20:47:26 -0500

    el base url ahora es una variable de entorno

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,7 +6,8 @@ import {
   CheckTokenResponse,
 } from "../types/auth";
 
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL ?? "http://localhost:3000";
 
 const apiClient: AxiosInstance = axios.create({
   baseURL: API_BASE_URL,
