94691252c31c8d7c46461f2be4cbe504c902c086
Laura <laura.nagamine@utec.edu.pe>
2025-11-19 20:52:47 -0500

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/frontend/',
+    base: '/frontend',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
