22de43b06f0bcddf3197808a3bb6f6d54faf12e0
Laura <laura.nagamine@utec.edu.pe>
2025-11-22 18:11:19 -0500

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/frontend',
+    base: '/KumoAppfrontend',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
